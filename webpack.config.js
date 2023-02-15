d10c238258d595ac49fd478e65d2bc432be53f19
himprover <hwang.improver@gmail.com>
2023-02-15 17:27:18 +0900

    fix: add missing configuration that typescript [#2]
    1. add @babel/preset-typescript
    2. add google typescript style guide prettier
    3. absolute path to src

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,42 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const path = require('path');
 const DotenvWebpackPlugin = require('dotenv-webpack');
 
 const PORT = 3000;
 
 module.exports = {
-	entry: './src/index.ts',
-	resolve: {
-		extensions: ['.ts', '.js'],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.ts?$/,
-				use: ['babel-loader', 'ts-loader'],
-			},
-			{ test: /\.js?$/, use: ['babel-loader'] },
-			{ test: /\.css?$/, use: ['style-loader', 'css-loader'] },
-		],
-	},
-	output: {
-		path: path.join(__dirname, '/dist'),
-		filename: 'bundle.js',
-	},
-	plugins: [
-		new CleanWebpackPlugin(),
-		new HtmlWebpackPlugin({
-			template: './src/index.html',
-		}),
-		new DotenvWebpackPlugin(),
-	],
-	devServer: {
-		historyApiFallback: true,
-		port: PORT,
-		hot: true,
-	},
+  entry: path.resolve(__dirname, './src/index.ts'),
+  resolve: {
+    extensions: ['.ts', '.js'],
+    alias: {
+      src: path.resolve(__dirname, 'src/'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ['babel-loader', 'ts-loader'],
+      },
+      {test: /\.js$/, use: ['babel-loader']},
+      {test: /\.css$/, use: ['style-loader', 'css-loader']},
+    ],
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new DotenvWebpackPlugin(),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    port: PORT,
+    hot: true,
+  },
 };
