3dc1292549af38ba12146301f9be53aeb62169af
himprover <hwang.improver@gmail.com>
2023-02-16 15:14:38 +0900

    feat: change example data, style [#3]
    1. change data schema of dynamic list
    2. add no-scrolling class

--- src/pages/main.ts
+++ src/pages/main.ts
@@ -1,5 +1,4 @@
 import './main.css';
-import './dynamicList.css';
 
 import {DynamicList} from 'src/components/dynamicList';
 
@@ -7,10 +6,10 @@ export const MainPage = () => {
   const main = document.getElementById('root');
 
   const dynamicList = new DynamicList('ul');
-  const exampleData = Array.from(
-    {length: 100},
-    (_, index) => `hello I'm ${index}.`
-  );
+  const exampleData = Array.from({length: 100}, (_, index) => ({
+    sequence: index + 1,
+    content: `hello I'm ${index + 1}.`,
+  }));
   dynamicList.addList(exampleData);
 
   if (dynamicList.$) {
