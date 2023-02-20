abf7a7d7abb6201ac007637c62911eb53a8dfc26
himprover <hwang.improver@gmail.com>
2023-02-20 09:45:44 +0900

    style: remove annotation about unuse code [#7]

--- src/components/List.test.ts
+++ src/components/List.test.ts
@@ -22,20 +22,4 @@ describe('[COMPONENT] List test', () => {
     expect(App).toHaveTextContent('item-1');
     expect(App).toHaveTextContent('item-10');
   });
-
-  // test('show popup when click to list item', async () => {
-  //   // given
-  //   const listItem: ListItemProps[] = Array.from({length: 10}, (_, index) => ({
-  //     sequence: index + 1,
-  //     content: `item-${index + 1}`,
-  //   }));
-  //   render(App, List, {list: listItem});
-  //   const exampleListItem = getByText(App, 'item-2').parentElement;
-  //   console.log(exampleListItem);
-  //   // when
-  //   await userEvent.click(exampleListItem!);
-  //   // // then
-  //   // const currentListItem = getByText(App, 'item-1').parentElement!;
-  //   // expect(currentListItem.classList.contains('active')).toBeTruthy();
-  // });
 });
