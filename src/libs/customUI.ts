fa8b287ad86215ab5efa6edebcc9def92c05c115
himprover <hwang.improver@gmail.com>
2023-02-26 23:03:06 +0900

    feat: useState setter callback [#9]
    1. setState can use callback function

--- src/libs/customUI.ts
+++ src/libs/customUI.ts
@@ -1,9 +1,10 @@
-import {Component} from 'src/types/component';
+import {Component} from 'src/types';
 
 export type UseStateType<T> = (
   initState: T
 ) => [state: T, setState: SetStateType<T>];
-export type SetStateType<T> = (state: T) => void;
+export type SetStateType<T> = (state: T | SetStateCallbackType<T>) => void;
+export type SetStateCallbackType<T> = (currentState: T) => T;
 
 export interface CustomContextProps {
   states: any[];
@@ -31,15 +32,16 @@ function CustomUI() {
 
     const state = states[currentStateCount];
 
-    function setState(newState: T) {
-      if (
-        state === newState ||
-        JSON.stringify(newState) === JSON.stringify(state)
-      ) {
-        return;
+    function setState(newState: T | SetStateCallbackType<T>) {
+      let _newState = newState;
+
+      if (isSetStateCallback(newState)) {
+        _newState = newState(states[currentStateCount]);
       }
 
-      states[currentStateCount] = newState;
+      if (!isStateChanged(_newState, state)) return;
+
+      states[currentStateCount] = _newState;
       _render();
     }
     context.currentStateCount += 1;
@@ -48,12 +50,13 @@ function CustomUI() {
 
   function _render() {
     const {target, component, props} = context;
+    if (!target || !component) return;
 
-    if (!target || !component) {
-      return;
-    }
+    const Component = component(props);
+    if (Component === null) return;
+
     target.innerHTML = '';
-    target.appendChild(component(props).render());
+    target.appendChild(Component.render());
     context.currentStateCount = 0;
     context.renderCount += 1;
   }
@@ -64,7 +67,22 @@ function CustomUI() {
     context.props = props;
     _render();
   }
+
   return {useState, render};
+}
+
+function isStateChanged<T>(currentState: T, newState: T) {
+  if (
+    currentState === newState ||
+    JSON.stringify(currentState) === JSON.stringify(newState)
+  )
+    return false;
+
+  return true;
+}
+
+function isSetStateCallback(args: any): args is SetStateCallbackType<any> {
+  return args.call !== undefined;
 }
 
 export const {useState, render} = CustomUI();
